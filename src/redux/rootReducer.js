b8d5f3f744d68fc126268a01251853d3dd79402f
Paras Anand <qtparas@gmail.com>
2020-07-21 06:32:25 +0530

    reset, deleted all the files.

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { countReducer } from "../containers/counter/reducers";
-import { repoReducer } from "../containers/git-repo/reducer";
-
-export const rootReducer = combineReducers({
-  count: countReducer,
-  repos: repoReducer,
-});
--- src/redux/rootSaga.js
+++
@@ -1,9 +0,0 @@
-import { all } from "redux-saga/effects";
-import {
-  gitUserWatcherSaga,
-  fetchUserDetails,
-} from "../containers/git-repo/sagas";
-
-export function* rootSaga() {
-  yield all([gitUserWatcherSaga(), fetchUserDetails()]);
-}
