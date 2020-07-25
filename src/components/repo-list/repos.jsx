354eade36041300713348cb3e2ace953dcacb39e
Paras Anand <qtparas@gmail.com>
2020-07-25 22:51:35 +0530

    added areEqual

--- src/components/repo-list/repos.jsx
+++ src/components/repo-list/repos.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Commit } from "../commit-list/commit-list";
 
+// function areEqual(prevProps, nextProps) {
+//   console.log(JSON.stringify(prevProps.userData.details) !== JSON.stringify(nextProps.userData.details))
+//   return (JSON.stringify(prevProps.userData.details) !== JSON.stringify(nextProps.userData.details))
+// }
+
+
 export const Repo = ({
   userData: { isLoading: isDetailLoading, isError: isDetailError, details },
 
