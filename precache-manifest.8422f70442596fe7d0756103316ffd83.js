7fad65536b150509177894825ff5b36f8c566150
Paras Anand <qtparas@gmail.com>
2020-07-29 03:42:46 +0530

    Updates

---
+++ precache-manifest.8422f70442596fe7d0756103316ffd83.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a1ee75ab9e5e5b4403b139d7deac4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a4711ab932c4b31f06b",
+    "url": "/static/css/main.01e07b1e.chunk.css"
+  },
+  {
+    "revision": "16f0ce0b3d7b59b60044",
+    "url": "/static/js/0.99623656.chunk.js"
+  },
+  {
+    "revision": "1855d953e64baa971e8f",
+    "url": "/static/js/1.090b2a90.chunk.js"
+  },
+  {
+    "revision": "b3d8c82b0ada67a899d9",
+    "url": "/static/js/2.1b523cf5.chunk.js"
+  },
+  {
+    "revision": "7a4711ab932c4b31f06b",
+    "url": "/static/js/main.4a386657.chunk.js"
+  },
+  {
+    "revision": "493f1b5ef13423a0edae",
+    "url": "/static/js/runtime-main.1a314f15.js"
+  },
+  {
+    "revision": "9e7b39695c99f7605120",
+    "url": "/static/js/vendors-combined.b4775217.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/vendors-combined.b4775217.chunk.js.LICENSE.txt"
+  }
+]);
