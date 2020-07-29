a1898c33d5842a5a06b05c0e0c47a7fd6a4eb6e9
Paras Anand <qtparas@gmail.com>
2020-07-30 05:01:28 +0530

    updated css

--- src/common/header/header.jsx
+++ src/common/header/header.jsx
@@ -4,8 +4,8 @@ export function Header() {
     return (
         <header>
             <h1>Github Reader</h1>
-            <h2>Counter: Keep Counting</h2>
-            <h3>Get Users List</h3>
+            <h2 className="hide-on-mobile">Counter: Keep Counting</h2>
+            <h3 className="hide-on-mobile">Get Users List</h3>
         </header>
 
     )
