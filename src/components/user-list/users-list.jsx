c50c3489a42997b8c63e79be7acbc5e22a886156
Paras Anand <qtparas@gmail.com>
2020-07-30 05:09:05 +0530

    added loader for github users also

--- src/components/user-list/users-list.jsx
+++ src/components/user-list/users-list.jsx
@@ -2,6 +2,7 @@ import React, { memo } from "react";
 import { connect } from "react-redux";
 import * as actions from "../../containers/git-repo/action";
 import { Repo } from "../repo-list/repos";
+import Loader from "react-loader-spinner";
 
 function areEqual(prevProps, nextProps) {
   return (JSON.stringify(prevProps) === JSON.stringify(nextProps))
@@ -11,29 +12,36 @@ export const Users = memo(connect(
   mapPropsToState,
   actions
 )(
-  (({ users, fetchUserDetails }) => (
-    <ul className="check-list">
-      {users &&
-        users.map(({ login, node_id }) => {
-          return (
-            <li
-              key={node_id}
-              onClick={(e) => {
-                e.stopPropagation();
-                fetchUserDetails(login);
-              }}
-            >
-              {login}
-              <Repo login={login} />
-            </li>
-          );
-        })}
-    </ul>
+  (({ users, fetchUserDetails, isLoading }) => (
+    isLoading ?
+      <Loader
+        type="TailSpin"
+        color="#333"
+        height={30}
+        width={30}
+      /> :
+      <ul className="check-list">
+        {users &&
+          users.map(({ login, node_id }) => {
+            return (
+              <li
+                key={node_id}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  fetchUserDetails(login);
+                }}
+              >
+                {login}
+                <Repo login={login} />
+              </li>
+            );
+          })}
+      </ul>
   )
   ), areEqual))
 
-function mapPropsToState({ repos: { users } }) {
-  return { users };
+function mapPropsToState({ repos: { users, isLoading } }) {
+  return { users, isLoading };
 }
 
 Users.displayName = "Users";
